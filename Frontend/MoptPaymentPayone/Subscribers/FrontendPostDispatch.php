6e4b8b73365302aa6687f4ba2e391cc0766e30fd
Stefan Müller <stefan.mueller@fatchip.de>
2017-03-16 12:11:38 +0100

    fixed: some data fields were accidently overwritten, because they were initialized in a loop

--- Frontend/MoptPaymentPayone/Subscribers/FrontendPostDispatch.php
+++ Frontend/MoptPaymentPayone/Subscribers/FrontendPostDispatch.php
@@ -268,6 +268,8 @@ class FrontendPostDispatch implements SubscriberInterface
 
         $paymentMeans = Shopware()->Modules()->Admin()->sGetPaymentMeans();
         $groupedPaymentMeans = false;
+        $data['moptPayolutionInformation'] = null;
+        $data['moptRatepayConfig'] = null;
 
         if ($controllerName && $controllerName === 'checkout') {
             $groupedPaymentMeans = $moptPayoneMain->getPaymentHelper()->groupCreditcards($paymentMeans);
@@ -307,7 +309,7 @@ class FrontendPostDispatch implements SubscriberInterface
             }
 
 
-            $data['moptPayolutionInformation'] = null;
+
             //prepare additional Payolution information and retrieve birthday from user data
             if ($moptPayoneMain->getPaymentHelper()->isPayonePayolutionDebitNote($paymentMean['name']) || $moptPayoneMain->getPaymentHelper()->isPayonePayolutionInvoice($paymentMean['name']) || $moptPayoneMain->getPaymentHelper()->isPayonePayolutionInstallment($paymentMean['name'])
             ) {
@@ -342,7 +344,6 @@ class FrontendPostDispatch implements SubscriberInterface
                 }
             }
 
-            $data['moptRatepayConfig'] = null;
             //prepare additional Ratepay information and retrieve birthday from user data
             if ($moptPayoneMain->getPaymentHelper()->isPayoneRatepayInvoice($paymentMean['name'])) {
                 $data['moptRatepayConfig'] = $moptPayoneMain->getPayoneConfig($paymentMean['id']);
