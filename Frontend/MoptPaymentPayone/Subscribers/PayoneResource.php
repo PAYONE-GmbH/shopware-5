83e96a1570a6521db9cc8f24e5040bf66d96ea98
Mario Dorn <mario.dorn@fatchip.de>
2017-10-09 16:28:51 +0200

    SW-109: Add a service that gives access to capture/refund methods

--- Frontend/MoptPaymentPayone/Subscribers/PayoneResource.php
+++ Frontend/MoptPaymentPayone/Subscribers/PayoneResource.php
@@ -14,28 +14,38 @@ class PayoneResource implements SubscriberInterface
      */
     public static function getSubscribedEvents()
     {
-        return array(
+        return [
             'Enlight_Bootstrap_InitResource_MoptPayoneMain' => 'onInitResourcePayoneMain',
-            'Enlight_Bootstrap_InitResource_MoptPayoneBuilder' => 'onInitResourcePayoneBuilder'
-        );
+            'Enlight_Bootstrap_InitResource_MoptPayoneBuilder' => 'onInitResourcePayoneBuilder',
+            'Enlight_Bootstrap_InitResource_payone_service' => 'onInitResourcePayoneService',
+        ];
+    }
+
+    /**
+     * @param \Enlight_Event_EventArgs $args
+     * @return \Mopt_PayoneService
+     */
+    public function onInitResourcePayoneService(\Enlight_Event_EventArgs $args)
+    {
+        return new \Mopt_PayoneService();
     }
     
   /**
    * Creates and returns the payone builder for an event.
    *
    * @param \Enlight_Event_EventArgs $args
-   * @return \Shopware_Components_Payone_Builder
+   * @return \Payone_Builder
    */
     public function onInitResourcePayoneBuilder(\Enlight_Event_EventArgs $args)
     {
         $payoneConfig = new \Payone_Config();
-        $logger = array('Payone_Protocol_Logger_Log4php' => null);
+        $logger = ['Payone_Protocol_Logger_Log4php' => null];
     
         $payoneConfig->setValue('api/default/protocol/loggers', $logger);
         $payoneConfig->setValue('transaction_status/default/protocol/loggers', $logger);
         $payoneConfig->setValue('session_status/default/protocol/loggers', $logger);
        
-      //$payoneConfig->setValue('api/default/protocol/filter/mask_value/percent', 50);
+        //$payoneConfig->setValue('api/default/protocol/filter/mask_value/percent', 50);
         
         $builder = new \Payone_Builder($payoneConfig);
         return $builder;
@@ -45,7 +55,7 @@ class PayoneResource implements SubscriberInterface
    * Creates and returns the payone builder for an event.
    *
    * @param \Enlight_Event_EventArgs $args
-   * @return \Shopware_Components_Payone_Builder
+   * @return \Mopt_PayoneMain
    */
     public function onInitResourcePayoneMain(\Enlight_Event_EventArgs $args)
     {
