ddf3aea83f775fad3c68f1d3fd22d41c90ce0ab1
roman <roman.wedemeier@fatchip.de>
2020-02-18 15:42:06 +0100

    SW-284: Add parameter to paydata

--- Frontend/MoptPaymentPayone/Controllers/Frontend/MoptPaymentPayone.php
+++ Frontend/MoptPaymentPayone/Controllers/Frontend/MoptPaymentPayone.php
@@ -756,6 +756,7 @@ class Shopware_Controllers_Frontend_MoptPaymentPayone extends Shopware_Controlle
         $session->offsetUnset('moptPaymentReference');
         $session->offsetUnset('isIdealredirect');
         $session->offsetUnset('moptPaypalInstallmentWorkerId');
+        $session->offsetUnset('paySafeToken');
     }
 
     /**
@@ -1098,6 +1099,9 @@ class Shopware_Controllers_Frontend_MoptPaymentPayone extends Shopware_Controlle
         $paydata->addItem(new Payone_Api_Request_Parameter_Paydata_DataItem(
             array('key' => 'payment_type', 'data' => $paymenttype)
         ));
+        $paydata->addItem(new Payone_Api_Request_Parameter_Paydata_DataItem(
+            array('key' => 'analysis_session_id', 'data' => Shopware()->Session()->get('paySafeToken'))
+        ));
 
         if ($paymentData['mopt_payone__payolution_b2bmode']) {
             $paydata->addItem(new Payone_Api_Request_Parameter_Paydata_DataItem(
@@ -1229,6 +1233,7 @@ class Shopware_Controllers_Frontend_MoptPaymentPayone extends Shopware_Controlle
     public function payolutionErrorAction()
     {
         $session = Shopware()->Session();
+        $session->offsetUnset('paySafeToken');
         $errorCode = $session->payolutionErrorCode;
         $errorMessage = $session->payolutionErrorMsg;
         $this->View()->errormessage = Shopware()->Snippets()->getNamespace('frontend/MoptPaymentPayone/errorMessages')->get('errorMessage' . $errorCode, $errorMessage . ' (Fehler ' . $session->payolutionErrorCode . ')', true);
