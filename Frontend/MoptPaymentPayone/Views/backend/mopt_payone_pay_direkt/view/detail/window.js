e4d042cb7e2da91e26c324fd2a2884e72e0b3f20
Stefan Mueller <stefstar@stefstar.de>
2023-01-12 10:48:54 +0100

    Revert unintended changes

--- Frontend/MoptPaymentPayone/Views/backend/mopt_payone_pay_direkt/view/detail/window.js
+++
@@ -1,7 +0,0 @@
-Ext.define('Shopware.apps.MoptPayonePayDirekt.view.detail.Window', {
-    extend: 'Shopware.window.Detail',
-    alias: 'widget.mopt-payone-pay-direkt-detail-window',
-    title : '{s name="details/title"}Payone Pay Direkt Details{/s}',
-    height: 650,
-    width: 900
-});
