976d54f5b0fd9e0ec1b761629a8145696db96e5a
smueller <stefan.mueller@fatchip.de>
2023-09-26 09:52:04 +0200

    SW-443: fixed more empty ratepay parameters

--- Frontend/MoptPaymentPayone/Components/Classes/PayoneFormHandler.php
+++ Frontend/MoptPaymentPayone/Components/Classes/PayoneFormHandler.php
@@ -776,7 +776,7 @@ class Mopt_PayoneFormHandler
 
         $paymentData['formData']['mopt_payone__ratepay_shopid'] = $formData['mopt_payone__ratepay_installment_shopid'];
         $paymentData['formData']['mopt_payone__ratepay_installment_device_fingerprint'] = $formData['mopt_payone__ratepay_installment_device_fingerprint'];
-        $paymentData['formData']['mopt_payone__ratepay_installment_installment_amount'] = $formData['mopt_payone__ratepay_installment_amount'];
+        $paymentData['formData']['mopt_payone__ratepay_installment_amount'] = $formData['mopt_payone__ratepay_installment_amount'];
         $paymentData['formData']['mopt_payone__ratepay_installment_total'] = $formData['mopt_payone__ratepay_installment_total'];
         $paymentData['formData']['mopt_payone__ratepay_installment_last_installment_amount'] = $formData['mopt_payone__ratepay_installment_last_installment_amount'];
         $paymentData['formData']['mopt_payone__ratepay_installment_interest_rate'] = $formData['mopt_payone__ratepay_installment_interest_rate'];
